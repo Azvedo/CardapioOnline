f3b72f33a96b23f0e111fd226df5d7138490350b
Rafael <raas@cin.ufpe.br>
2024-12-31 15:38:01 -0300

    tirando o deslizamento horizontal

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer className="bg-brand-primary text-white text-center p-4">
+      <p> by:RafaelAzevedo </p>
+    </footer>
+  );
+}
+
+export default Footer;
--- src/screens/Order.jsx
+++ src/screens/Order.jsx
@@ -1,12 +1,14 @@
 import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 function Order() {
 
     return (
-      <>
+      <div className="font-Poppins overflow-x-hidden">
         <Header/>
         <h1>Order</h1>
-      </>
+        <Footer/>
+      </div>
     )
   }
   
